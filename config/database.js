56e1b4d74adef78bf18a47686b5ed2be7848280e
MohammedAliBelay <eng.mame17@gmail.com>
2026-01-26 19:36:57 +0300

    done

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const database = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.MYSQL_DB,
-  port: Number(process.env.DB_HOST_PORT),
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
